513275434f36eb54121f78fb150839f1ff3cc375
wangqi <pengzhu.hi@163.com>
2023-11-15 16:20:13 +0800

    Update dw.py

--- dw.py
+++ dw.py
@@ -119,3 +119,97 @@ configuration.verify_ssl = False
 client = kubernetes.client.ApiClient(configuration)
 v1 = kubernetes.client.CoreV1Api(client)
 v1.list_node()
+
+
+###
+import random
+import time
+import threading
+import numpy as np
+from milvus import Milvus, IndexType
+from milvus.client.types import MetricType
+
+SERVER_ADDR = "127.0.0.1"
+SERVER_PORT = '19531'
+COLLECTION_NAME = "TEST"
+COLLECTION_DIMENSION = 450
+COLLECTION_PARAM = {'collection_name': "",
+                    'dimension': COLLECTION_DIMENSION,
+                    'index_file_size': 1024,
+                    'metric_type':MetricType.L2}
+INDEX_TYPE = IndexType.IVF_SQ8
+INDEX_PARAM = {'nlist': 512}
+
+MILVUS = Milvus(host=SERVER_ADDR, port=SERVER_PORT)
+
+def gen_vec_list(nb, seed=np.random.RandomState(1234)):
+    xb = seed.rand(nb, COLLECTION_DIMENSION).astype("float32")
+    vec_list = xb.tolist()
+    return vec_list
+
+def create_collection(name = COLLECTION_NAME):
+    param = COLLECTION_PARAM
+    param['collection_name'] = name
+    status, has = MILVUS.has_collection(collection_name=name)
+    if not has:
+        status = MILVUS.create_collection(param)
+        MILVUS.create_index(collection_name=name, index_type=INDEX_TYPE, params=INDEX_PARAM)
+        print("create", name)
+
+def drop_collection(name = COLLECTION_NAME):
+    status = MILVUS.drop_collection(collection_name=name)
+    print("drop", name)
+
+def get_collection_stats(name = COLLECTION_NAME):
+    status, stat = MILVUS.get_collection_stats(collection_name=name)
+    print(stat)
+
+def insert(name = COLLECTION_NAME):
+    batch = 100
+    for i in range(100000):
+        vectors = gen_vec_list(batch)
+        status, ids = MILVUS.insert(collection_name=name, records=vectors)
+        print("insert", i, len(ids))
+        time.sleep(0.2)
+
+if __name__ == "__main__":
+    create_collection()
+    insert()
+
+###
+import random
+import time
+import threading
+import numpy as np
+from milvus import Milvus, IndexType
+from milvus.client.types import MetricType
+
+SERVER_ADDR = "127.0.0.1"
+SERVER_PORT = '19531'
+COLLECTION_NAME = "TEST"
+
+NQ = 1
+SEARCH_PARAM = {'nprobe': 4}
+
+MILVUS = Milvus(host=SERVER_ADDR, port=SERVER_PORT)
+
+def search():
+    for i in range(1000000):
+        q_records = [[random.random() for _ in range(COLLECTION_DIMENSION)] for _ in range(1)]
+        topk = random.randint(20,1200)
+        status, results = MILVUS.search(collection_name=COLLECTION_NAME, query_records=q_records, top_k=topk,
+                                        params=SEARCH_PARAM)
+        print("search", i, 'tok=', topk)
+        if not status.OK():
+            break
+
+if __name__ == "__main__":
+    threads = []
+    for k in range(10):
+        x = threading.Thread(target=search, args=())
+        threads.append(x)
+        x.start()
+
+    for th in threads:
+        th.join()
+
